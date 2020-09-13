5cad75c7c9a306b47718cc8c200167768db6002e
mertmtn <mertmetin1993@gmail.com>
2020-09-14 01:35:13 +0300

    Initial Commit

---
+++ PuppeteerSharp.ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using PuppeteerShap.Methods;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PuppeteerSharp.ConsoleApp
+{
+    public class Program
+    {
+        public static async System.Threading.Tasks.Task Main(string[] args)
+        {
+            var response= await PuppeteerMethods.LoginFacebook();            
+      
+            if (response.IsSuccess)
+            {
+                Console.WriteLine(response.Message);
+            }
+            else
+            {
+                Console.WriteLine(response.Message);
+            }
+            Console.ReadKey();
+        }
+    }
+}
