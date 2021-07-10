183fc8ffc22711bfe297e9e51fc6adcee9b23fbd
MERT METİN <mertmetin1993@gmail.com>
2021-07-11 01:25:49 +0300

    Console Uygulamasında metot oluşturuldu.

--- PuppeteerSharp.ConsoleApp/Program.cs
+++ PuppeteerSharp.ConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using PuppeteerSharp.Methods; 
-using System; 
+using System;
+using System.Threading.Tasks;
 
 namespace PuppeteerSharp.ConsoleApp
 {
@@ -7,17 +8,22 @@ namespace PuppeteerSharp.ConsoleApp
     {
         public static async System.Threading.Tasks.Task Main(string[] args)
         {
-            var response= await PuppeteerMethods.GetTitleOfPage();            
-      
+            await GetTitleOfPage();
+            Console.ReadKey();
+        }
+
+        private static async Task GetTitleOfPage()
+        {
+            var response = await PuppeteerMethods.GetTitleOfPage();
+
             if (response.Success && !string.IsNullOrEmpty(response.Data))
             {
-                Console.WriteLine(response.Data);
+                Console.WriteLine("Sayfa Başlığı:" + response.Data);
             }
             else
             {
                 Console.WriteLine(response.Message);
             }
-            Console.ReadKey();
         }
     }
 }
